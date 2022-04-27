9c5eb75feedb71f036e4cb7d8661eb6cbb3c0828
ehajjTTP <92182730+ehajjTTP@users.noreply.github.com>
2022-04-27 16:05:47 -0400

    Add files via upload

---
+++ apple_Pie/apple_Pie/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  apple_Pie
+//
+//  Created by Emira Hajj on 4/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet var buttons: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
